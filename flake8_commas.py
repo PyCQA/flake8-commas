6e3109413d30793e1ddaf82d15e276e761d2e8cb
Dan Palmer <dan@danpalmer.me>
2016-03-25 00:18:02 +0000

    Support non-standard named kwargs.
    This means checking for the ** rather than the "kwargs", which is more
    robust and correct in other ways too.
    
    Unfortunately, this would mean adding a "last_last_last_token" and thats
    starting to get nasty, so this refactors the checks to just inspect the
    list of tokens and jump positions in that.
    
    Comments have to be filtered out beforehand otherwise they could be an
    arbitrary number of tokens between `idx` and the ** or newline we're
    checking for.

--- flake8_commas.py
+++ flake8_commas.py
@@ -47,27 +47,20 @@ class CommaChecker(object):
 
     def get_comma_errors(self, file_contents):
         tokens = [Token(t) for t in tokenize.generate_tokens(lambda L=iter(file_contents): next(L))]
+        tokens = [t for t in tokens if t.type != tokenize.COMMENT]
 
-        last_last_token = None
-        last_token = None
-        for token in tokens:
-            if token.type == tokenize.COMMENT:
-                continue
-
+        for idx, token in enumerate(tokens):
             if (token.string in self.CLOSING_BRACKETS and
-                    last_token and last_token.type == tokenize.NL and
-                    last_last_token and last_last_token.string != ',' and
-                    last_last_token.string != 'kwargs'):
+                    (idx - 1 > 0) and tokens[idx - 1].type == tokenize.NL and
+                    (idx - 2 > 0) and tokens[idx - 2].string != ',' and
+                    (idx - 3 > 0) and tokens[idx - 3].string != '**'):
 
-                end_row, end_col = last_last_token.end
+                end_row, end_col = tokens[idx - 2].end
                 yield {
                     'message': '%s %s' % (COMMA_ERROR_CODE, COMMA_ERROR_MESSAGE),
                     'line': end_row,
                     'col': end_col,
                 }
-
-            last_last_token = last_token
-            last_token = token
 
 
 class Token:
