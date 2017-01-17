06e95c73fcbf58b15a7b61cb6c3b084c7c2dde71
Thomas Grainger <tom.grainger@procensus.com>
2017-01-17 11:26:02 +0000

    Move most code to ._base and outside of CommaChecker class

---
+++ flake8_commas/_base.py
@@ -0,0 +1,293 @@
+import tokenize
+import collections
+import token as mod_token
+
+try:
+    import pycodestyle
+except ImportError:
+    import pep8 as pycodestyle
+import pkg_resources
+
+try:
+    dist = pkg_resources.get_distribution('flake8-commas')
+    __version__ = dist.version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
+
+# A parenthesized expression list yields whatever that expression list
+# yields: if the list contains at least one comma, it yields a tuple;
+# otherwise, it yields the single expression that makes up the expression
+# list.
+
+PYTHON_2_KWDS = {
+    'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+    'else', 'except', 'exec', 'finally', 'for', 'from', 'global', 'if',
+    'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'print', 'raise',
+    'return', 'try', 'while', 'with', 'yield',
+}
+
+PYTHON_3_KWDS = {
+    'False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class',
+    'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for',
+    'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not',
+    'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield',
+}
+
+KWD_LIKE_FUNCTION = {'import', 'assert'}
+
+ALL_KWDS = (PYTHON_2_KWDS & PYTHON_3_KWDS) - KWD_LIKE_FUNCTION
+NOT_PYTHON_2_KWDS = (PYTHON_3_KWDS - PYTHON_2_KWDS) - KWD_LIKE_FUNCTION
+NOT_PYTHON_3_KWDS = (PYTHON_2_KWDS - PYTHON_3_KWDS) - KWD_LIKE_FUNCTION
+
+
+class TupleOrParenthForm(object):
+    def __bool__(self):
+        return False
+
+    __nonzero__ = __bool__
+
+
+TUPLE_OR_PARENTH_FORM = TupleOrParenthForm()
+
+
+class SimpleToken(object):
+    def __init__(self, token, type):
+        self.token = token
+        self.type = type
+
+
+NEW_LINE = 'new-line'
+COMMA = ','
+OPENING_BRACKET = '('
+SOME_CLOSING = 'some-closing'
+SOME_OPENING = 'some-opening'
+OPENING = {SOME_OPENING,  OPENING_BRACKET}
+CLOSING = {SOME_CLOSING}
+BACK_TICK = '`'
+CLOSE_ATOM = CLOSING | {BACK_TICK}
+FOR = 'for'
+NAMED = 'named'
+PY2_ONLY_ERROR = 'py2-only-error'
+PY3K_ONLY_ERROR = 'py3-only-error'
+DEF = 'def'
+FUNCTION_DEF = 'function-def'
+FUNCTION = {NAMED, PY2_ONLY_ERROR, PY3K_ONLY_ERROR, FUNCTION_DEF}
+UNPACK = '* or **'
+NONE = SimpleToken(token=None, type=None)
+
+
+def get_type(token):
+    type = token.type
+    if type == tokenize.NL:
+        return NEW_LINE
+
+    string = token.string
+    if type == tokenize.NAME and string == 'for':
+        return FOR
+    if type == tokenize.NAME and string == 'def':
+        return DEF
+    if type == mod_token.NAME and string not in ALL_KWDS:
+        if string in NOT_PYTHON_2_KWDS:
+            return PY2_ONLY_ERROR
+        if string in NOT_PYTHON_3_KWDS:
+            return PY3K_ONLY_ERROR
+        return NAMED
+    if string in {'**', '*'}:
+        return UNPACK
+    if string == ',':
+        return COMMA
+    if string == '(':
+        return OPENING_BRACKET
+    if string in {'[', '{'}:
+        return SOME_OPENING
+    if string in {']', ')', '}'}:
+        return SOME_CLOSING
+    if string == '`':
+        return BACK_TICK
+    return
+
+
+def simple_tokens(tokens):
+    tokens = (t for t in tokens if t.type != tokenize.COMMENT)
+
+    token = next(tokens)
+    previous_token = SimpleToken(token=token, type=get_type(token))
+    for token in tokens:
+        next_token = SimpleToken(token=token, type=get_type(token))
+        if previous_token.type == NEW_LINE and next_token.type == NEW_LINE:
+            continue
+        yield previous_token
+        previous_token = next_token
+
+
+ERRORS = {
+    True: ('C812', 'missing trailing comma'),
+    FUNCTION_DEF: ('C812', 'missing trailing comma'),
+    PY3K_ONLY_ERROR: ('C813', 'missing trailing comma in Python 3'),
+    PY2_ONLY_ERROR: ('C814', 'missing trailing comma in Python 2'),
+    'py35': ('C815', 'missing trailing comma in Python 3.5+'),
+}
+
+
+def process_parentheses(token, prev_1, prev_2):
+    previous_token = prev_1
+
+    if token.type == OPENING_BRACKET:
+        is_function = (
+            previous_token and
+            (
+                (previous_token.type in CLOSE_ATOM) or
+                (
+                    previous_token.type in FUNCTION
+                )
+            )
+        )
+        if is_function:
+            if prev_2.type == DEF:
+                return [FUNCTION_DEF]
+            tk_string = previous_token.type
+            if tk_string == PY2_ONLY_ERROR:
+                return [PY2_ONLY_ERROR]
+            if tk_string == PY3K_ONLY_ERROR:
+                return [PY3K_ONLY_ERROR]
+        else:
+            return [TUPLE_OR_PARENTH_FORM]
+
+    return [True]
+
+
+def get_file_contents(filename):
+    if filename == 'stdin':
+        return pycodestyle.stdin_get_value().splitlines(True)
+    else:
+        return pycodestyle.readlines(filename)
+
+
+def get_tokens(file_contents):
+    file_contents_iter = iter(file_contents)
+
+    def file_contents_next():
+        return next(file_contents_iter)
+
+    for t in tokenize.generate_tokens(file_contents_next):
+        yield Token(t)
+
+
+def no_qa_comment(token):
+    return token.type == tokenize.COMMENT and token.string.endswith('noqa')
+
+
+def get_noqa_lines(file_contents):
+    tokens = get_tokens(file_contents)
+    return [token.start_row for token in tokens if no_qa_comment(token)]
+
+
+def get_comma_errors(file_contents):
+    tokens = simple_tokens(get_tokens(file_contents))
+
+    valid_comma_context = [False]
+
+    window = collections.deque([NONE, NONE, NONE], maxlen=4)
+
+    for token in tokens:
+        window.append(token)
+        prev_3, prev_2, prev_1, _ = window
+        if token.type in OPENING:
+            valid_comma_context.extend(
+                process_parentheses(token, prev_1, prev_2),
+            )
+
+        if token.type == FOR:
+            valid_comma_context[-1] = False
+
+        comma_found = (
+            valid_comma_context[-1] == TUPLE_OR_PARENTH_FORM and
+            token.type == COMMA
+        )
+        if comma_found:
+            valid_comma_context[-1] = True
+
+        comma_required = (
+            token.type in CLOSING and
+            valid_comma_context[-1] and
+            prev_1.type == NEW_LINE and
+            prev_2.type != COMMA and
+            prev_2.type not in OPENING and
+            (prev_3.type != UNPACK or (
+                prev_3.type == UNPACK and
+                valid_comma_context[-1] != FUNCTION_DEF))
+        )
+        if comma_required:
+            end_row, end_col = prev_2.token.end
+            if (prev_3.type == UNPACK):
+                errors = ERRORS['py35']
+            else:
+                errors = ERRORS[valid_comma_context[-1]]
+            yield {
+                'message': '%s %s' % errors,
+                'line': end_row,
+                'col': end_col,
+            }
+
+        if token.type in CLOSING:
+            valid_comma_context.pop()
+
+
+class CommaChecker(object):
+    name = __name__
+    version = __version__
+
+    def __init__(self, tree, filename='(none)', builtins=None):
+        fn = 'stdin' if filename in ('stdin', '-', None) else filename
+        self.filename = fn
+
+    def run(self):
+        file_contents = get_file_contents(self.filename)
+
+        noqa_line_numbers = get_noqa_lines(file_contents)
+        for error in get_comma_errors(file_contents):
+            if error.get('line') not in noqa_line_numbers:
+                yield (
+                    error.get('line'),
+                    error.get('col'),
+                    error.get('message'),
+                    type(self),
+                )
+
+
+class Token:
+    '''Python 2 and 3 compatible token'''
+    def __init__(self, token):
+        self.token = token
+
+    @property
+    def type(self):
+        return self.token[0]
+
+    @property
+    def string(self):
+        return self.token[1]
+
+    @property
+    def start(self):
+        return self.token[2]
+
+    @property
+    def start_row(self):
+        return self.start[0]
+
+    @property
+    def start_col(self):
+        return self.start[1]
+
+    @property
+    def end(self):
+        return self.token[3]
+
+    @property
+    def end_row(self):
+        return self.end[0]
+
+    @property
+    def end_col(self):
+        return self.end[1]
--- test/test_checks.py
+++ test/test_checks.py
@@ -1,172 +1,205 @@
-from flake8_commas import CommaChecker
 import os
-from unittest import TestCase
+
+from flake8_commas._base import (
+    get_file_contents, get_noqa_lines, get_comma_errors,
+)
+
+C813 = 'C813 missing trailing comma in Python 3'
+C814 = 'C814 missing trailing comma in Python 2'
+C815 = 'C815 missing trailing comma in Python 3.5+'
 
 
-class TestChecks(TestCase):
-    def test_get_noqa_lines(self):
-        checker = CommaChecker(None, filename=get_absolute_path('data/no_qa.py'))
-        self.assertEqual(checker.get_noqa_lines(checker.get_file_contents()), [2])
+def test_get_noqa_lines():
+    filename = get_absolute_path('data/no_qa.py')
+    assert get_noqa_lines(get_file_contents(filename)) == [2]
 
 
-class CommaTestChecks(TestCase):
-    def test_one_line_dict(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/one_line_dict.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_multiline_good_dict(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/multiline_good_dict.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_multiline_bad_dict(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/multiline_bad_dict.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 14, 'line': 2, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_bad_list(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/bad_list.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 5, 'line': 4, 'message': 'C812 missing trailing comma'},
-            {'col': 5, 'line': 10, 'message': 'C812 missing trailing comma'},
-            {'col': 5, 'line': 17, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_bad_function_call(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/bad_function_call.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 17, 'line': 3, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_multiline_bad_function_def(self):
-        fixture = 'data/multiline_bad_function_def.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 13, 'line': 9, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_bad_function_one_param(self):
-        fixture = 'data/multiline_bad_function_one_param.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 13, 'line': 2, 'message': 'C812 missing trailing comma'},
-            {'col': 9, 'line': 8, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_good_empty_comma_context(self):
-        fixture = 'data/good_empty_comma_context.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_comma_good_dict(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/comment_good_dict.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_no_comma_required_list_comprehension(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/list_comprehension.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_no_comma_required_dict_comprehension(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/dict_comprehension.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_no_comma_required_multiline_if(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/multiline_if.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_comma_required_after_unpack_in_non_def_python_3_5(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/unpack.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 12, 'line': 26, 'message': 'C815 missing trailing comma in Python 3.5+'},
-            {'col': 23, 'line': 32, 'message': 'C815 missing trailing comma in Python 3.5+'},
-            {'col': 14, 'line': 39, 'message': 'C812 missing trailing comma'},
-            {'col': 12, 'line': 46, 'message': 'C815 missing trailing comma in Python 3.5+'},
-            {'col': 12, 'line': 50, 'message': 'C815 missing trailing comma in Python 3.5+'},
-            {'col': 9, 'line': 58, 'message': 'C815 missing trailing comma in Python 3.5+'},
-            {'col': 9, 'line': 62, 'message': 'C815 missing trailing comma in Python 3.5+'},
-        ])
-
-    def test_no_comma_required_in_parenth_form(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path('data/parenth_form.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_comma_required_in_argument_list(self):
-        fixture = 'data/callable_before_parenth_form.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 7, 'line': 7, 'message': 'C812 missing trailing comma'},
-            {'col': 7, 'line': 15, 'message': 'C812 missing trailing comma'},
-            {'col': 7, 'line': 23, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_comma_required_even_if_you_use_or(self):
-        fixture = 'data/multiline_bad_or_dict.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 14, 'line': 3, 'message': 'C812 missing trailing comma'},
-        ])
-
-    def test_comma_not_required_even_if_you_use_dict_for(self):
-        fixture = 'data/multiline_good_single_keyed_for_dict.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_comma_not_required_in_parenth_form_string_splits(self):
-        fixture = 'data/multiline_string.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
-
-    def test_comma_not_required_in_comment_lines(self):
-        fixture = 'data/good_list.py'
-        comma_checker = CommaChecker(None, filename=get_absolute_path(fixture))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
+def test_one_line_dict():
+    filename = get_absolute_path('data/one_line_dict.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == []
 
 
-class ParenthFormChecks(TestCase):
-    base = 'data/keyword_before_parenth_form/'
+def test_multiline_good_dict():
+    filename = get_absolute_path('data/multiline_good_dict.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == []
 
-    def test_base(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'base.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
 
-    def test_base_bad(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'base_bad.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 5, 'line': 2, 'message': 'C812 missing trailing comma'},
-            {'col': 10, 'line': 8, 'message': 'C812 missing trailing comma'},
-            {'col': 7, 'line': 14, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 17, 'message': 'C812 missing trailing comma'},
-            {'col': 7, 'line': 21, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 24, 'message': 'C812 missing trailing comma'},
-        ])
+def test_multiline_bad_dict():
+    filename = get_absolute_path('data/multiline_bad_dict.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 14, 'line': 2, 'message': 'C812 missing trailing comma'},
+    ]
 
-    def test_py2(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'py2.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
 
-    def test_py2_bad(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'py2_bad.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 7, 'line': 4, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 7, 'message': 'C814 missing trailing comma in Python 2'},
-            {'col': 7, 'line': 12, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 15, 'message': 'C814 missing trailing comma in Python 2'},
-            {'col': 7, 'line': 20, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 23, 'message': 'C814 missing trailing comma in Python 2'},
-            {'col': 7, 'line': 28, 'message': 'C812 missing trailing comma'},
-            {'col': 11, 'line': 31, 'message': 'C814 missing trailing comma in Python 2'},
-        ])
+def test_bad_list():
+    filename = get_absolute_path('data/bad_list.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 5, 'line': 4, 'message': 'C812 missing trailing comma'},
+        {'col': 5, 'line': 10, 'message': 'C812 missing trailing comma'},
+        {'col': 5, 'line': 17, 'message': 'C812 missing trailing comma'},
+    ]
 
-    def test_py2_3(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'py2_3.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [
-            {'col': 9, 'line': 4, 'message': 'C813 missing trailing comma in Python 3'},
-            {'col': 9, 'line': 8, 'message': 'C813 missing trailing comma in Python 3'},
-        ])
 
-    def test_py3(self):
-        comma_checker = CommaChecker(None, filename=get_absolute_path(self.base + 'py3.py'))
-        self.assertEqual(list(comma_checker.get_comma_errors(comma_checker.get_file_contents())), [])
+def test_bad_function_call():
+    filename = get_absolute_path('data/bad_function_call.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 17, 'line': 3, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_multiline_bad_function_def():
+    fixture = 'data/multiline_bad_function_def.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 13, 'line': 9, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_bad_function_one_param():
+    fixture = 'data/multiline_bad_function_one_param.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 13, 'line': 2, 'message': 'C812 missing trailing comma'},
+        {'col': 9, 'line': 8, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_good_empty_comma_context():
+    fixture = 'data/good_empty_comma_context.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_comma_good_dict():
+    fixture = 'data/comment_good_dict.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_no_comma_required_list_comprehension():
+    fixture = 'data/list_comprehension.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_no_comma_required_dict_comprehension():
+    fixture = 'data/dict_comprehension.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_no_comma_required_multiline_if():
+    fixture = 'data/multiline_if.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_comma_required_after_unpack_in_non_def_python_3_5():
+    fixture = 'data/unpack.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 12, 'line': 26, 'message': C815},
+        {'col': 23, 'line': 32, 'message': C815},
+        {'col': 14, 'line': 39, 'message': 'C812 missing trailing comma'},
+        {'col': 12, 'line': 46, 'message': C815},
+        {'col': 12, 'line': 50, 'message': C815},
+        {'col': 9, 'line': 58, 'message': C815},
+        {'col': 9, 'line': 62, 'message': C815},
+    ]
+
+
+def test_no_comma_required_in_parenth_form():
+    fixture = 'data/parenth_form.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_comma_required_in_argument_list():
+    fixture = 'data/callable_before_parenth_form.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 7, 'line': 7, 'message': 'C812 missing trailing comma'},
+        {'col': 7, 'line': 15, 'message': 'C812 missing trailing comma'},
+        {'col': 7, 'line': 23, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_comma_required_even_if_you_use_or():
+    fixture = 'data/multiline_bad_or_dict.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 14, 'line': 3, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_comma_not_required_even_if_you_use_dict_for():
+    fixture = 'data/multiline_good_single_keyed_for_dict.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_comma_not_required_in_parenth_form_string_splits():
+    fixture = 'data/multiline_string.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_comma_not_required_in_comment_lines():
+    fixture = 'data/good_list.py'
+    filename = get_absolute_path(fixture)
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+base = 'data/keyword_before_parenth_form/'
+
+
+def test_base():
+    filename = get_absolute_path(base + 'base.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_base_bad():
+    filename = get_absolute_path(base + 'base_bad.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 5, 'line': 2, 'message': 'C812 missing trailing comma'},
+        {'col': 10, 'line': 8, 'message': 'C812 missing trailing comma'},
+        {'col': 7, 'line': 14, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 17, 'message': 'C812 missing trailing comma'},
+        {'col': 7, 'line': 21, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 24, 'message': 'C812 missing trailing comma'},
+    ]
+
+
+def test_py2():
+    filename = get_absolute_path(base + 'py2.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == []
+
+
+def test_py2_bad():
+    filename = get_absolute_path(base + 'py2_bad.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 7, 'line': 4, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 7, 'message': C814},
+        {'col': 7, 'line': 12, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 15, 'message': C814},
+        {'col': 7, 'line': 20, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 23, 'message': C814},
+        {'col': 7, 'line': 28, 'message': 'C812 missing trailing comma'},
+        {'col': 11, 'line': 31, 'message': C814},
+    ]
+
+
+def test_py2_3():
+    filename = get_absolute_path(base + 'py2_3.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == [
+        {'col': 9, 'line': 4, 'message': C813},
+        {'col': 9, 'line': 8, 'message': C813},
+    ]
+
+
+def test_py3():
+    filename = get_absolute_path(base + 'py3.py')
+    assert list(get_comma_errors(get_file_contents(filename))) == []
 
 
 def get_absolute_path(filepath):
