0fca3d14ae172811d9cb6e21acb69973ebd17f1a
Thomas Grainger <tom.grainger@procensus.com>
2017-01-18 15:40:01 +0000

    Support flake8 3.x.x Fixes #20

--- test/test_checks.py
+++ test/test_checks.py
@@ -1,7 +1,7 @@
 import os
 
 from flake8_commas._base import (
-    get_file_contents, get_noqa_lines, get_comma_errors,
+    get_tokens, get_noqa_lines, get_comma_errors,
 )
 
 C813 = 'C813 missing trailing comma in Python 3'
@@ -11,29 +11,29 @@ C815 = 'C815 missing trailing comma in Python 3.5+'
 
 def test_get_noqa_lines():
     filename = get_absolute_path('data/no_qa.py')
-    assert get_noqa_lines(get_file_contents(filename)) == [2]
+    assert get_noqa_lines(get_tokens(filename)) == [2]
 
 
 def test_one_line_dict():
     filename = get_absolute_path('data/one_line_dict.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_multiline_good_dict():
     filename = get_absolute_path('data/multiline_good_dict.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_multiline_bad_dict():
     filename = get_absolute_path('data/multiline_bad_dict.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 14, 'line': 2, 'message': 'C812 missing trailing comma'},
     ]
 
 
 def test_bad_list():
     filename = get_absolute_path('data/bad_list.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 5, 'line': 4, 'message': 'C812 missing trailing comma'},
         {'col': 5, 'line': 10, 'message': 'C812 missing trailing comma'},
         {'col': 5, 'line': 17, 'message': 'C812 missing trailing comma'},
@@ -42,7 +42,7 @@ def test_bad_list():
 
 def test_bad_function_call():
     filename = get_absolute_path('data/bad_function_call.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 17, 'line': 3, 'message': 'C812 missing trailing comma'},
     ]
 
@@ -50,7 +50,7 @@ def test_bad_function_call():
 def test_multiline_bad_function_def():
     fixture = 'data/multiline_bad_function_def.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 13, 'line': 9, 'message': 'C812 missing trailing comma'},
     ]
 
@@ -58,7 +58,7 @@ def test_multiline_bad_function_def():
 def test_bad_function_one_param():
     fixture = 'data/multiline_bad_function_one_param.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 13, 'line': 2, 'message': 'C812 missing trailing comma'},
         {'col': 9, 'line': 8, 'message': 'C812 missing trailing comma'},
     ]
@@ -67,37 +67,37 @@ def test_bad_function_one_param():
 def test_good_empty_comma_context():
     fixture = 'data/good_empty_comma_context.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_comma_good_dict():
     fixture = 'data/comment_good_dict.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_no_comma_required_list_comprehension():
     fixture = 'data/list_comprehension.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_no_comma_required_dict_comprehension():
     fixture = 'data/dict_comprehension.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_no_comma_required_multiline_if():
     fixture = 'data/multiline_if.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_comma_required_after_unpack_in_non_def_python_3_5():
     fixture = 'data/unpack.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 12, 'line': 26, 'message': C815},
         {'col': 23, 'line': 32, 'message': C815},
         {'col': 14, 'line': 39, 'message': 'C812 missing trailing comma'},
@@ -112,13 +112,13 @@ def test_comma_required_after_unpack_in_non_def_python_3_5():
 def test_no_comma_required_in_parenth_form():
     fixture = 'data/parenth_form.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_comma_required_in_argument_list():
     fixture = 'data/callable_before_parenth_form.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 7, 'line': 7, 'message': 'C812 missing trailing comma'},
         {'col': 7, 'line': 15, 'message': 'C812 missing trailing comma'},
         {'col': 7, 'line': 23, 'message': 'C812 missing trailing comma'},
@@ -128,7 +128,7 @@ def test_comma_required_in_argument_list():
 def test_comma_required_even_if_you_use_or():
     fixture = 'data/multiline_bad_or_dict.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 14, 'line': 3, 'message': 'C812 missing trailing comma'},
     ]
 
@@ -136,19 +136,19 @@ def test_comma_required_even_if_you_use_or():
 def test_comma_not_required_even_if_you_use_dict_for():
     fixture = 'data/multiline_good_single_keyed_for_dict.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_comma_not_required_in_parenth_form_string_splits():
     fixture = 'data/multiline_string.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_comma_not_required_in_comment_lines():
     fixture = 'data/good_list.py'
     filename = get_absolute_path(fixture)
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 base = 'data/keyword_before_parenth_form/'
@@ -156,12 +156,12 @@ base = 'data/keyword_before_parenth_form/'
 
 def test_base():
     filename = get_absolute_path(base + 'base.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_base_bad():
     filename = get_absolute_path(base + 'base_bad.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 5, 'line': 2, 'message': 'C812 missing trailing comma'},
         {'col': 10, 'line': 8, 'message': 'C812 missing trailing comma'},
         {'col': 7, 'line': 14, 'message': 'C812 missing trailing comma'},
@@ -173,12 +173,12 @@ def test_base_bad():
 
 def test_py2():
     filename = get_absolute_path(base + 'py2.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def test_py2_bad():
     filename = get_absolute_path(base + 'py2_bad.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 7, 'line': 4, 'message': 'C812 missing trailing comma'},
         {'col': 11, 'line': 7, 'message': C814},
         {'col': 7, 'line': 12, 'message': 'C812 missing trailing comma'},
@@ -192,7 +192,7 @@ def test_py2_bad():
 
 def test_py2_3():
     filename = get_absolute_path(base + 'py2_3.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == [
+    assert list(get_comma_errors(get_tokens(filename))) == [
         {'col': 9, 'line': 4, 'message': C813},
         {'col': 9, 'line': 8, 'message': C813},
     ]
@@ -200,7 +200,7 @@ def test_py2_3():
 
 def test_py3():
     filename = get_absolute_path(base + 'py3.py')
-    assert list(get_comma_errors(get_file_contents(filename))) == []
+    assert list(get_comma_errors(get_tokens(filename))) == []
 
 
 def get_absolute_path(filepath):
