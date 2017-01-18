9ae01c77cb2f98d4da078bad6fd6fc2fff1eb8c4
Thomas Grainger <tom.grainger@procensus.com>
2017-01-18 11:14:11 +0000

    require 2 <= flake8 < 3, Ref #20

--- setup.py
+++ setup.py
@@ -26,9 +26,7 @@ setup(
     maintainer='Thomas Grainger',
     maintainer_email='flake8-commas@graingert.co.uk',
     version='0.3.1.dev0',
-    install_requires=[
-        'flake8',
-    ],
+    install_requires=['flake8>=2, <3.0.0'],
     url='https://github.com/flake8-commas/flake8-commas/',
     long_description=readme + '\n\n' + history,
     description='Flake8 lint for trailing commas.',
