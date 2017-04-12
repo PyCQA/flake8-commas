a00d1a16b0606f9ca228cde6244e5c3350d2274b
Peter Law <peter@thread.com>
2017-04-12 17:10:58 +0100

    More test cases

--- test/data/multiline_index_access.py
+++ test/data/multiline_index_access.py
@@ -2,6 +2,14 @@ multiline_index_access[
     "good"
 ]
 
+multiline_index_access_after_function()[
+    "good"
+]
+
+multiline_index_access_after_inline_index_access['first'][
+    "good"
+]
+
 multiline_index_access[
     "probably fine",
 ]
