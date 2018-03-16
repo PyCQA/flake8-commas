227150e172cc4b8f8caaa09047c5d0f301936abe
Chris AtLee <chris@atlee.ca>
2018-03-16 05:40:54 -0400

    Simplify tests

--- test/data/bare.py
+++ test/data/bare.py
@@ -12,23 +12,8 @@ foo = (
 3,
 4,
 )
-bar = 10,
 
-foo = 3,
-bar = 10,
-foo_bar = 2,
-
-foo = 3,
-bar = 10
-foo_bar = 2,
-
-foo = 3,
-bar = 10,
-foo_bar = 2
-
-foo = 3 
-bar = 10 
-foo_bar = 2,
+foo = 3, 
 
 class A(object):
  foo = 3
@@ -36,29 +21,8 @@ class A(object):
  foo_bar = 2
 
 a = ('a',)
+
 from foo import bar, baz
-
-a = ('a',)
-('b', 'c')
-
-a = ('a',)
-('b', 'c')
-
-
-"""
-foo
-"""
-bah = 1
-
-base_url = reverse(
-'test',
-args=(pk,)
-)
-
-base_url = reverse(
-'test',
-args=pk,
-)
 
 group_by = function_call('arg'),
 
